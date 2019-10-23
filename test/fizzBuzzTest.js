dd5cc3357cc5e228dc55413462a3e1e71d70b8fd
jmotosp <jmotosp@gmail.com>
2019-10-23 19:15:02 +0200

    refactor to extract fizzBuzz variable

--- test/fizzBuzzTest.js
+++ test/fizzBuzzTest.js
@@ -3,15 +3,15 @@ const chai = require('chai'),
     FizzBuzz = require('../src/fizzBuzz');
 
 describe('Unit Test battery of FizzBuzz', () => {
+    let fizzBuzz = new FizzBuzz();
+
     it('when 3 return Fizz', () => {
-        let fizzBuzz = new FizzBuzz(),
-            result = fizzBuzz.run(3),
+        let result = fizzBuzz.run(3),
             expected = 'Fizz';
         result.should.be.eql(expected);
     });
     it('when 5 return Buzz', () => {
-        let fizzBuzz = new FizzBuzz(),
-            result = fizzBuzz.run(5),
+        let result = fizzBuzz.run(5),
             expected = 'Buzz';
         result.should.be.eql(expected);
     });
